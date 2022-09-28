94b16f611e3764bffc262b2de1ac7651c6a8209d
Thong La Nguyen <supergogito1@gmail.com>
2022-09-28 11:58:19 +0700

    Add other files

---
+++ neovim/src/nvim/sign.h
@@ -0,0 +1,15 @@
+#ifndef NVIM_SIGN_H
+#define NVIM_SIGN_H
+
+#include <stdbool.h>
+
+#include "nvim/buffer_defs.h"
+#include "nvim/eval/funcs.h"
+#include "nvim/ex_cmds_defs.h"
+#include "nvim/sign_defs.h"
+
+
+#ifdef INCLUDE_GENERATED_DECLARATIONS
+# include "sign.h.generated.h"
+#endif
+#endif  // NVIM_SIGN_H
